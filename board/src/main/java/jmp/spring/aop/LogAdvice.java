7bcb5e4b0177e83e87c101a8891a6dfb5a37c922
Hansat8342 <junghyunmin9729@gmail.com>
2021-04-29 15:11:40 +0900

    댓글 업데이트 및 보드 백업 프로세스 작성
    aop 오류 정리 및 댓글 개수 표시
    댓글 실시간 업데이트

--- board/src/main/java/jmp/spring/aop/LogAdvice.java
+++ board/src/main/java/jmp/spring/aop/LogAdvice.java
@@ -1,6 +1,5 @@
 package jmp.spring.aop;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,18 +14,34 @@ import lombok.extern.log4j.Log4j;
 public class LogAdvice {
 
 			//execution(수식어(생략가능 public, private..) 리턴타입 클래스이름(생략가능, 쓸경우 풀패키지명) 매서드이름(파라미터)
-	@Before("execution(* jmp.spring.service.BoardService.*(..))") 
-	public void logBefore(JoinPoint jp) {
-		log.info("AOP=================");
-	}
-
-	
-
-	@Around("execution(* jmp.spring.service.BoardService.*(..))")
-	public void logTime(ProceedingJoinPoint pjp) {
-
-		log.info("logTime==========================");
-		// 타겟 객체의 실행 전, 후의 시간을 측정하여 실행시간을 로그에 기록합니다.
-	}
+//	@Before("execution(* jmp.spring.service.BoardService.*(..))") 
+//	public void logBefore() {
+//		log.info("AOP=================");
+//	}
+//
+//	
+//
+//	@Around("execution(* jmp.spring.service.BoardService.*(..))")
+//	public Object logTime(ProceedingJoinPoint pjp) {
+//			
+//			// @before  메소드 실행전 시간
+//			long startTime = System.currentTimeMillis();
+//			
+//			log.info("============"+pjp.getTarget());
+//			Object res = null;
+//			try {
+//				res = pjp.proceed();
+//			} catch (Throwable e) {
+//				// @AfterThrowing
+//				e.printStackTrace();
+//			}
+//			
+//			long endTime = System.currentTimeMillis();
+//			// @after  메소드 실행 후 시간
+//			// 메소드의 실행 시간을 측정하여 로그로 남겨줍니다.
+//			log.info("============ 실행시간 : " + (endTime-startTime));
+//	
+//			return res;
+//		}
 
 }
--- board/src/main/java/jmp/spring/service/ReplyServiceImpl.java
+++ board/src/main/java/jmp/spring/service/ReplyServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import jmp.spring.mapper.ReplyMapper;
 import jmp.spring.vo.Criteria;
@@ -16,21 +17,41 @@ public class ReplyServiceImpl implements ReplyService {
 	ReplyMapper mapper;
 	
 	@Override
+	@Transactional
 	public int insert(ReplyVo vo) {
 		// TODO Auto-generated method stub
-		return mapper.insert(vo);
+		int res = mapper.insert(vo);
+		// 댓글 갯수는 입력할때, 삭제할때 변경된다.
+		// 댓글에 갯수를 카운트 해서 tbl_board 테이블의 replycnt 컬럼에 입력 update
+		// 갯수가 변경되는  게시글의 번호는 vo.getBno();
+		mapper.updateReplyCnt(vo.getBno());
+		return res;
 	}
 
 	@Override
 	public int update(ReplyVo vo) {
 		// TODO Auto-generated method stub
-		return mapper.update(vo);
+		int res = mapper.update(vo);
+		// 댓글수를 카운트 하여 테이블에 저장;
+		mapper.updateReplyCnt(vo.getBno());
+		return res;
 	}
 
 	@Override
+	@Transactional
 	public int delete(int rno) {
 		// TODO Auto-generated method stub
-		return mapper.delete(rno);
+		// 먼저 조회후 삭제
+		ReplyVo vo = mapper.get(rno);
+		
+		int res = mapper.delete(rno);
+		// 댓글 삭제시 댓글의 수가 변경됨 > 댓글 수를 다시 카운트
+		// reply 상세 정보 획득
+		
+		// 상세정보의 bno 값을 획득
+		mapper.updateReplyCnt(vo.getBno());
+		
+		return res;
 	}
 
 	@Override
